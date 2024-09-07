b90819a1922cb3bbe0afb732c91772a98110774a
Alperen Özkan <alperenweb3@gmail.com>
2024-09-07 18:40:29 +0200

    chore: required assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+  title: 'Property Webapp | Next.js 14',
+  keywords: 'rental, property, real estate',
+  description: 'Next.js project for property webapp',
+};
+
 function Layout({ children }) {
   return (
     <html>
